06f2995243d66211a69ae867c4e967379be1bfc3
NikAfz <nikafz980@gmail.com>
2024-08-28 21:00:38 +0330

    feat : mui, router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Landing from './pages/Landing'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Landing/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+        <Route path='*' element={<Landing/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
