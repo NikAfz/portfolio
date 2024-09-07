86f3995c178b4569ba123a0022bce41867da57d1
NikAfz <nikafz980@gmail.com>
2024-09-07 12:17:40 +0330

    fix : about for portfolio

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,10 @@
+function AboutMe(props) {
+  return (
+    <div className="about--container">
+      <div className="about--img">image</div>
+      <div className="about-text--container">text</div>
+    </div>
+  );
+}
+
+export default AboutMe;
