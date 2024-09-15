27cb41343259a7a768cc89f7987fc55e782c3441
NikAfz <nikafz980@gmail.com>
2024-09-15 15:47:07 +0330

    fix : page links

--- src/components/PageLink.jsx
+++ src/components/PageLink.jsx
@@ -1,6 +1,22 @@
 function PageLink(props) {
   return (
     <div className="page-link--container">
+      <li className="page-link--item">
+        <a href="#hero"> {"<NikAfz/>"}</a>
+      </li>
+      <li className="page-link--item">
+        <a href="#about"> {"<AboutMe/>"}</a>
+      </li>
+      <li className="page-link--item">
+        <a href="#skills"> {"<Skills/>"}</a>
+      </li>
+      <li className="page-link--item">
+        <a href="#"> {"<Projects/>"}</a>
+      </li>
+      <li className="page-link--item">
+        <a href="#"> {"<ContactMe/>"}</a>
+      </li>
+
       <div className="social--line"></div>
     </div>
   );
