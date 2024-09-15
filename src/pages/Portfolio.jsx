dee835ba234f128cdf3e885a0f358228d8682ca8
NikAfz <nikafz980@gmail.com>
2024-09-15 15:46:38 +0330

    fix : portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import SocialLink from "../components/SocialLink";
 import AboutMe from "../components/AboutMe";
 import PageLink from "../components/PageLink";
+import Skills from "../components/Skills";
 
 function Portfolio(props) {
   return (
@@ -11,6 +12,7 @@ function Portfolio(props) {
       <PageLink />
       <Hero />
       <AboutMe />
+      <Skills />
     </>
   );
 }
