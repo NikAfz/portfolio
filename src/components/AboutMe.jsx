03e59b2c13e07f4c2090354b35f2fb30125a7b56
NikAfz <nikafz980@gmail.com>
2024-09-22 11:01:39 +0330

    fix : about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,6 @@
 function AboutMe(props) {
   return (
     <div className="about--container" id="about">
-      <div className="about--img">image</div>
-
       <div className="about-text--container">
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
@@ -12,29 +10,25 @@ function AboutMe(props) {
           velit corrupti. Ex at obcaecati ut accusantium! Nisi ullam mollitia
           natus vitae, officiis, aliquam odit architecto tempora veritatis porro
           expedita facilis perferendis corporis eaque. Commodi ex autem sequi
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-          obcaecati blanditiis dolor rem corrupti voluptate nisi asperiores,
-          placeat quia repellendus.
         </p>
-        <div className="about-info--container">
-          <div className="about-info">
-            <p className="info--title">Name :</p>
-            <p className="info--dis">NikAfz</p>
-          </div>
-          <div className="about-info">
-            <p className="info--title">Education :</p>
-            <p className="info--dis">diploma</p>
-          </div>
-          <div className="about-info">
-            <p className="info--title">Mail :</p>
-            <p className="info--dis">nikafz980@gmail.com</p>
-          </div>
-          <div className="about-info">
-            <p className="info--title">Employment :</p>
-            <p className="info--dis">open</p>
-          </div>
+      </div>
+
+      <div className="about-info--container">
+        <div className="about-info">
+          <p className="info--title">Name :</p>
+          <p className="info--dis">NikAfz</p>
+        </div>
+        <div className="about-info">
+          <p className="info--title">Education :</p>
+          <p className="info--dis">diploma</p>
+        </div>
+        <div className="about-info">
+          <p className="info--title">Mail :</p>
+          <p className="info--dis">nikafz980@gmail.com</p>
+        </div>
+        <div className="about-info">
+          <p className="info--title">Employment :</p>
+          <p className="info--dis">open</p>
         </div>
       </div>
     </div>
